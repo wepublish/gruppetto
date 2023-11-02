95649807b8f8a0aa81dd42053bc7cf85f84013e8
LaurentAe <95348542+LaurentAe@users.noreply.github.com>
2023-11-02 08:10:46 +0100

    Delte unused tsx-File

--- apps/gruppetto/src/gruppetto-inner-teaser-wrapper.tsx
+++
@@ -1,6 +0,0 @@
-import {styled, css} from '@mui/material'
-import {TeaserInnerWrapper} from '@wepublish/website'
-
-export const GruppettoTeaserInnerWrapper = styled(TeaserInnerWrapper)`
-  date: none;
-`
